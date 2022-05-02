3160744b7df8a19432a74ca521588e3d6d3b79f4
Diane Yeo <ystdiane26@gmail.com>
2022-05-02 21:10:45 +0800

    csv function and db function

--- Staycay/staycation/Q1/book_packages.py
+++ Staycay/staycation/Q1/book_packages.py
@@ -7,21 +7,21 @@ import csv
 product = Blueprint('product', __name__)
 
 
-# # render product page - reads from csv file
-# @product.route('/products')
-# @login_required
-# def book_package():
-#     # read staycation.csv file
-#     with open('assets/js/staycation.csv', 'r') as file:
-#         reader = csv.DictReader(file, delimiter=',', quotechar='"')
-#         packages = list(reader)
-#         # print(packages)
-#         return render_template('products.html', name=current_user.name, panel="Package", products=packages)
-
-
-# render product page - reads from db(monogodb)
+# render product page - reads from csv file
 @product.route('/products')
 @login_required
 def book_package():
-    packages = Staycation.objects.all()
-    return render_template('products.html', name=current_user.name, panel='Package', products=packages)
+    # read staycation.csv file
+    with open('assets/js/staycation.csv', 'r') as file:
+        reader = csv.DictReader(file, delimiter=',', quotechar='"')
+        packages = list(reader)
+        # print(packages)
+        return render_template('products.html', name=current_user.name, panel="Package", products=packages)
+
+
+# # render product page - reads from db(monogodb)
+# @product.route('/products')
+# @login_required
+# def book_package():
+#     packages = Staycation.objects.all()
+#     return render_template('products.html', name=current_user.name, panel='Package', products=packages)
